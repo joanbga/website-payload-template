9f5d03454a3231dca92a5e8c49336b93c02a07a4
joan brugaya <joan.brugaya@gmail.com>
2024-07-18 20:21:48 +0200

    added tailwind for both client and admin

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { navigationService } from '@/services/navigation.service'
 import { cacheWithTags } from '@/utils/cache'
+import './globals.css'
 
 const getNavs = cacheWithTags(navigationService.getNavigation, ['navigations'])
 export default async function RootLayout({
@@ -13,11 +14,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav>
+        <nav className="flex gap-2">
           {navs.map((nav) => (
-            <p key={nav.href}>
-              <Link href={`/${nav.isHomePage ? '' : nav.href}`}>{nav.label}</Link>
-            </p>
+            <Link
+              key={nav.href}
+              className="text-gray-500 hover:text-gray-700"
+              href={`/${nav.isHomePage ? '' : nav.href}`}
+            >
+              {nav.label}
+            </Link>
           ))}
         </nav>
         {children}
---
+++ src/components/Alert.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Alert() {
+  return <div className="text-red-500 p-4 bg-red-100 rounded-md">This is an alert</div>
+}
