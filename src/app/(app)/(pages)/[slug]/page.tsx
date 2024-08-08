c16ada886b2603d2f3132c9453f2f42de605401a
joan brugaya <joan.brugaya@gmail.com>
2024-08-08 14:16:42 +0200

    use page component

--- src/app/(app)/(pages)/[slug]/page.tsx
+++ src/app/(app)/(pages)/[slug]/page.tsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import { unstable_cache as cache } from 'next/cache'
-import { getPayloadHMR } from '@payloadcms/next/utilities'
-import configPromise from '@payload-config'
 import { notFound } from 'next/navigation'
 import { cacheWithTags } from '@/utils/cache'
 import { pagesService } from '@/services/pages.service'
+import Page from '@/components/Page'
 
 const getPageBySlug = cacheWithTags(pagesService.getPageBySlug, ['pages'])
 
 export default async function page({ params }: { params: { slug: string } }) {
   const page = await getPageBySlug(params.slug)
+
   if (!page) {
     notFound()
   }
-  return (
-    <>
-      <p>{page.title}</p>
-    </>
-  )
+  return <Page page={page} />
 }
 
 const getPagesSlugs = cacheWithTags(pagesService.getAllPageSlug, ['pages'])
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,18 @@
 import { type Page } from '@/payload-types'
 import React from 'react'
+import { Button } from './ui/button'
 
 type PagePros = {
   page: Page
 }
 
 export default function Page({ page }: PagePros) {
-  return <div>{page.title}</div>
+  return (
+    <div>
+      {page.title}
+      <p>
+        <Button>Click me</Button>
+      </p>
+    </div>
+  )
 }
